8f13232fe384677d8b9793e07a9db19810df72b8
Linda Zhang <zlindacz@gmail.com>
2016-08-30 18:47:25 -0700

    remove greeting from components, add app dir, AppHeaderContainer.js and AppHeader.jsx

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, hashHistory } from 'react-router';
 
-import GreetingContainer from './greeting/GreetingContainer';
+import AppHeaderContainer from './app/AppHeaderContainer';
 // import UserShowContainer from './users/UserShowContainer';
+import { receiveCurrentUser } from '../actions/session_actions'
 
-export const Root({store}) {
-  <Provider store={store}>
-    <Router history={hashHistory}>
-      {}
-    </Router>
-  </Provider>
+const Root({store}) {
+  const RequestCurrentUserShowOnEnter = () => {
+    store.dispatch(receiveCurrentUser());
+  };
+
+  return (
+    <Provider store={store}>
+      <Router history={hashHistory}>
+        <Route path="/" component={AppHeaderContainer} onEnter={RequestCurrentUserShowOnEnter}>
+
+        </Route>
+      </Router>
+    </Provider>
+  )
 }
+
+export default Root;
