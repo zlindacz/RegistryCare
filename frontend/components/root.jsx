2ad525a6c204769d73dc17cc79ab63c1cc7afca0
Linda Zhang <zlindacz@gmail.com>
2016-08-30 18:03:03 -0700

    organize components dir

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, hashHistory } from 'react-router';
 
+import GreetingContainer from './greeting/GreetingContainer';
+// import UserShowContainer from './users/UserShowContainer';
+
 export const Root({store}) {
   <Provider store={store}>
     <Router history={hashHistory}>
-      
+      {}
     </Router>
   </Provider>
 }
