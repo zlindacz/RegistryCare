47ad33e7acf252021293eb74664e10eda7158a44
Linda Zhang <zlindacz@gmail.com>
2016-09-01 21:07:23 -0700

    start of cloudinary

--- frontend/components/router.jsx
+++ frontend/components/router.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app';
-// import AppHeaderContainer from './app/app_header_container';
-// import { receiveCurrentUser } from '../actions/session_actions'
 import SignupForm from './signup/signup_form';
 
 const AppRouter = () => {
