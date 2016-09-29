e50b2c6be6d6bdb000c4793f225423041241ad5e
Linda Zhang <zlindacz@gmail.com>
2016-09-28 18:16:42 -0700

    add seeds

--- frontend/components/router/router.jsx
+++ frontend/components/router/router.jsx
@@ -3,7 +3,7 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from '../app';
 import SignupFormContainer from '../signup/signup_form_container';
 import UserIndexContainer from '../user/index/user_index_container';
-import UserPledgesContainer from '../user/pledge/user_pledges_container';
+// import UserPledgesContainer from '../user/pledge/user_pledges_container';
 import UserRegistryContainer from '../user/registry/user_registry_container';
 import HomeContainer from '../home/home_container';
 
@@ -36,7 +36,6 @@ class AppRouter extends React.Component{
           <Route path="/index" component={ UserIndexContainer } onEnter={ this._ensureLoggedIn } />
           <Route path="/signup" component={ SignupFormContainer } onEnter={ this._redirectIfLoggedIn }/>
           <Route path="/registry/:userId" component={ UserRegistryContainer } onEnter={ this._ensureLoggedIn } />
-          <Route path="/pledges" component={ UserPledgesContainer} onEnter={ this._ensureLoggedIn } />
         </Route>
       </Router>
     );
