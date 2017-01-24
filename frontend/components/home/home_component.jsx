ea48cbcba734f02e6705a96d12d8984b0613c2e3
Linda Zhang <zlindacz@gmail.com>
2017-01-23 20:27:15 -0800

    it is responsive!

--- frontend/components/home/home_component.jsx
+++ frontend/components/home/home_component.jsx
@@ -17,21 +17,22 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home">
-        <section className="home-quote-block">
-          <h1 className="home-tagline-top">giving</h1>
-          <h1 className="home-tagline-bottom">just got easier</h1>
-          <button className="home-tour-button" onClick={this.guestLogin}>Take a tour</button>
-        </section>
+        <div className="row">
+          <section className="home-quote-block col-12">
+            <h1 className="home-tagline-top">giving</h1>
+            <h1 className="home-tagline-bottom">just got easier</h1>
+            <button className="home-tour-button" onClick={this.guestLogin}>Take a tour</button>
+          </section>
+        </div>
 
         <section className="home-content-block">
           <div className="home-testimonials">
-
-            <div className="home-user">
-              <div className="quote">
+            <div className="home-user row">
+              <div className="quote col-4 offset-left-2">
                 <h2 className="home-testimonial-quote">I move a lot. Using Registry Care, I can find dozens of organizations in my local area that will take my gently using clothes, appliances, and furniture.</h2>
                 <h3 className="home-testimonial-source">- Linda in San Francisco</h3>
               </div>
-              <div className="home-testimonial-photo">
+              <div className="home-testimonial-photo col-4 offset-right-2">
                 <img src="https://res.cloudinary.com/zlindacz/image/upload/f_auto,q_72,w_300/v1475086346/furniture_yb6krb.jpg"
                      alt="photo of a single seat sofa on a street outside"
                      srcSet="https://res.cloudinary.com/zlindacz/image/upload/f_auto,q_72,w_300/v1475086346/furniture_yb6krb.jpg 650w,
@@ -40,26 +41,26 @@ class Home extends React.Component {
               </div>
             </div>
 
-            <div className="home-org">
-              <div className="home-testimonial-photo">
+            <div className="home-org row">
+              <div className="home-testimonial-photo col-4 offset-left-2">
                 <img src="https://res.cloudinary.com/zlindacz/image/upload/f_auto,q_72,w_300/v1475086345/cafeteria5_dmbfek.jpg"
                      alt="photo of a green-tiled cafeteria"
                      srcSet="https://res.cloudinary.com/zlindacz/image/upload/f_auto,q_72,w_650/v1475086345/cafeteria5_dmbfek.jpg 650w,
                              https://res.cloudinary.com/zlindacz/image/upload/f_auto,q_72,w_1500/v1475086345/cafeteria5_dmbfek.jpg 1500w"
                      sizes="(min-width: 1200px) 1500px, 100vw"/>
               </div>
-              <div className="quote">
+              <div className="quote col-4 offset-right-2">
                 <h2 className="home-testimonial-quote">Ever since we created our registry, our donation center has seen an increase in supplies every month. We are in the middle of expanding our program, so every bit helps.</h2>
                 <h3 className="home-testimonial-source">- For the Children in Ohio</h3>
               </div>
             </div>
 
-            <div className="why-quotes">
+            <div className="why-quotes center">
               <h2 className="home-quote">No act of kindness, no matter how small, is ever wasted</h2>
               <h3 className="home-quote-source">- Aesop</h3>
             </div>
 
-            <div className="last-bid">
+            <div className="last-bid center">
               <h2 className="give-today">Start giving today.</h2>
               <button className="home-tour-button2" onClick={this.guestLogin}>Try us out</button>
             </div>
