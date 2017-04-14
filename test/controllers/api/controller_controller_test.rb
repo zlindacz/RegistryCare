0566778a99b013d41aa280521db2d54faf87236a
Linda Zhang <zlindacz@gmail.com>
2017-04-14 10:28:28 -0700

    delete test dir. will make tests in future

--- test/controllers/api/controller_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Api::ControllerControllerTest < ActionController::TestCase
-  test "should get pledges" do
-    get :pledges
-    assert_response :success
-  end
-
-end
